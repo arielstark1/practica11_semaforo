62af7e68947009c6a9788bd97979c91aebabb77a
arielmarvin@gmail.com <arielmarvin@gmail.com>
2021-08-21 16:20:43 -0400

    cambioultimo

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import "./App.css";
-import React, { Component } from "react";
-
-export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.styles = {
-      fontSize: 30,
-      backgroundColor: "yellow",
-      fontWeight: "bold",
-    };
+import React, { useState } from "react";
+function App(){
+  let [encendido,setEncendido]=useState(0);
+  function on(color){
+    setEncendido(color)
   }
+  
+  return (
+    
+    <div className="carta">
+      {
+        encendido === "red"?
+          <button className="stop red-light"> stop</button>
+          :
+          <button onClick={()=>{on("red")}} className="stop"> stop</button>
+          
+        }
+        {
+          encendido ==="yellow"?
+          <button className="change yellow-light">change</button>
+          :
+          <button onClick={()=>{on("yellow")}} className="change"> change</button>
 
-  cambio() {}
-
-  render() {
-    return (
-      <div className="carta">
-        <button className="stop"> stop</button>
-        <button className="change">change </button>
-        <button className="go"> go </button>
-      </div>
-    );
-  }
+        }
+        {
+          encendido ==="green"?
+          <button className="go green-light">go </button>
+          :
+          <button onClick={()=>{on("green")}} className="go"> go</button>
+        }
+    </div>
+  );
 }
 
 export default App;
